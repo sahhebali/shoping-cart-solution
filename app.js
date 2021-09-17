8e844ec1667943a5ccc9875cb173fbbc223925a3
sahhebali <saheb.co21@gmail.com>
2021-09-17 16:01:24 +0530

    module-27.2-end

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-document.getElementById('case-plus').addEventListener('click', function () {
+function updateIncresing(incresing) {
     const caseInput = document.getElementById('case-number');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-    // const caseNumberInputText = parseInt(caseNumber);
-    // console.log(caseNumber);
+    let caseNumber = caseInput.value;
+    if (incresing) {
+        caseNumber = parseInt(caseNumber) + 1;
+    }
+    else if (caseNumber > 0) {
+        caseNumber = parseInt(caseNumber) - 1;
+    }
+    caseInput.value = caseNumber;
+    // update case total 
+    const caseTotal = document.getElementById('case-toatl');
+    caseTotal.innerText = caseNumber * 59;
+}
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateIncresing(true);
+
+})
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateIncresing(false);
+    // 
 
 })
