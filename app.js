ffd81ffb36c43f0d80fd899a2b6da8275d2e7f1b
sahhebali <saheb.co21@gmail.com>
2021-09-18 23:22:47 +0530

    subtoatl-total-commit-

--- app.js
+++ app.js
@@ -1,25 +1,52 @@
-function updateIncresing(incresing) {
-    const caseInput = document.getElementById('case-number');
-    let caseNumber = caseInput.value;
-    if (incresing) {
-        caseNumber = parseInt(caseNumber) + 1;
+function updatProductNumber(product, price, isIncresing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncresing) {
+        productNumber = parseInt(productNumber) + 1;
     }
-    else if (caseNumber > 0) {
-        caseNumber = parseInt(caseNumber) - 1;
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
-    // update case total 
-    const caseTotal = document.getElementById('case-toatl');
-    caseTotal.innerText = caseNumber * 59;
+    productInput.value = productNumber;
+    // update  total 
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+    // calculate total
+    calculateTotal();
 }
 
-document.getElementById('case-plus').addEventListener('click', function () {
-    updateIncresing(true);
+function getInputValue(product) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
 
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totyalPrice = subTotal + tax;
+
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totyalPrice;
+
+
+}
+
+// phone increase decrease event
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updatProductNumber('phone', 1229, true);
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updatProductNumber('phone', 1219, false);
+})
+// hsandle case-cover increase decrease event
+document.getElementById('case-plus').addEventListener('click', function () {
+    updatProductNumber('case', 59, true);
 })
 
 document.getElementById('case-minus').addEventListener('click', function () {
-    updateIncresing(false);
-    // 
-
+    updatProductNumber('case', 59, false);
 })
